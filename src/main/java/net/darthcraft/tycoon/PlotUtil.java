bb855978c59596424d5ca2b0df13887789803391
Patrick Seidel <psycowithespn1@aim.com>
2014-02-20 15:36:11 -0500

    Untested adding in plot hash getting from world location.

---
+++ src/main/java/net/darthcraft/tycoon/PlotUtil.java
@@ -0,0 +1,37 @@
+package net.darthcraft.tycoon;
+
+import net.darthcraft.tycoon.chunkgen.CGUtil;
+
+public class PlotUtil {
+
+    private static final int MAX_OFFSET = 30000000;
+    private static final int MAX_PLOT_OFFSET = MAX_OFFSET / CGUtil.GRID_CELL_SIZE;
+
+    public PlotCoords worldCoordsToPlotLoc(int x, int z) {
+        int nX = x / CGUtil.GRID_CELL_SIZE;
+        int nZ = z / CGUtil.GRID_CELL_SIZE;
+        return new PlotCoords(nX, nZ);
+    }
+
+    public static long worldCoordsToHash(int x, int z) {
+        int nX = (int) Math.floor(((double) x) / CGUtil.GRID_CELL_SIZE);
+        int nZ = (int) Math.floor(((double) z) / CGUtil.GRID_CELL_SIZE);
+        return plotLocToHash(nX, nZ);
+    }
+
+    public static long plotLocToHash(PlotCoords coords) {
+        return plotLocToHash(coords.getPlotX(), coords.getPlotZ());
+    }
+
+    public static long plotLocToHash(int x, int z) {
+        x += MAX_PLOT_OFFSET;
+        z += MAX_PLOT_OFFSET;
+        return ((long) x << 32) + z - Integer.MIN_VALUE;
+    }
+
+    public static PlotCoords plotCoordsFromHash(long hash) {
+        int x = (int) (hash >> 32) - MAX_PLOT_OFFSET;
+        int z = (int) (hash & 0xFFFFFFFF) + Integer.MIN_VALUE - MAX_PLOT_OFFSET;
+        return new PlotCoords(x, z);
+    }
+}
--- src/test/java/net/darthcraft/tycoon/PlotUtilTest.java
+++ src/test/java/net/darthcraft/tycoon/PlotUtilTest.java
@@ -3,29 +3,29 @@ package net.darthcraft.tycoon;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PlotHashTest {
+public class PlotUtilTest {
 
     @Test
     public void test() {
         PlotCoords coords = new PlotCoords(12345, 54321);
-        long hash = PlotHash.plotLocToHash(coords);
-        PlotCoords nCods = PlotHash.plotCoordsFromHash(hash);
+        long hash = PlotUtil.plotLocToHash(coords);
+        PlotCoords nCods = PlotUtil.plotCoordsFromHash(hash);
         Assert.assertEquals(coords, nCods);
     }
 
     @Test
     public void testZero() {
         PlotCoords coords = new PlotCoords(0, 0);
-        long hash = PlotHash.plotLocToHash(coords);
-        PlotCoords nCoords = PlotHash.plotCoordsFromHash(hash);
+        long hash = PlotUtil.plotLocToHash(coords);
+        PlotCoords nCoords = PlotUtil.plotCoordsFromHash(hash);
         Assert.assertEquals(coords, nCoords);
     }
 
     @Test
     public void testNegative() {
         PlotCoords coords = new PlotCoords(-12345, -54321);
-        long hash = PlotHash.plotLocToHash(coords);
-        PlotCoords nCoords = PlotHash.plotCoordsFromHash(hash);
+        long hash = PlotUtil.plotLocToHash(coords);
+        PlotCoords nCoords = PlotUtil.plotCoordsFromHash(hash);
         Assert.assertEquals(coords, nCoords);
     }
 }
